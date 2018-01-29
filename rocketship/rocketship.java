41a9603dec0b57d70f0ad8095eee3b0dc985df3c
John Luke Schaefer <lukeschaefer@gmail.com>
2018-01-28 22:24:42 -0800

    asd

--- rocketship/rocketship.java
+++ rocketship/rocketship.java
@@ -4,24 +4,36 @@ public class rocketship{
    }
    public static void rocket(int i){
       cone(i);
+      line(i);
       body1(i);
+      line(i);
       body2(i);
+      line(i);
       cone(i);
    }
+   
+   public static void line(int i){
+   	String line1 = "";
+   	for(int x = 0; x <= i; x++){
+   		line1 += "*=";
+   	}
+   	System.out.println("+" + line1 + "+");
+   }
+   
    public static void cone(int i){
    String fslash = "";
    String bslash = "";
    String spaces = "";
    
    for(int x = 0; x <= i; x++){
-      spaces += " ";
-   }
-   
-   for(int x = 0; x <= i; x++){
       fslash += "/";
       bslash += "\\";
+      spaces = "";
+      for(int y = (i-x-1); y >= 0; y--){
+      	spaces += " ";
+      }
       //I need to remove spaces from the spaces variable in order to make it work.
-      //The assignment prompt mentioned nested for loops; how do I get that going?
+      //The assignment prompt mentioned nested for loops; why?
       System.out.println(spaces + fslash + "**" + bslash);
    }
    
