8ee08e9469f3688a51a7d7533589b332c873cabe
John Luke Schaefer <lukeschaefer@gmail.com>
2018-01-29 18:46:59 -0800

    asd

--- cafewall/cafewall.java
+++ cafewall/cafewall.java
@@ -1,5 +1,15 @@
-public class cafewall{
+import java.awt.*;
 
+public class cafewall{
+   public static void main(String[] args){
+      DrawingPanel panel = new DrawingPanel(600,600);
+      Graphics g = panel.getGraphics();
+      g.setColor(Color.RED);
+      g.fillRect(17, 45, 139, 241);
+   }
+   public static void row(int pairs, int size, int xc, int yc){
+      
+   }
 
 
 
