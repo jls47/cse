564b46d7a313a1c80806310c2effc84c07896306
jls47 <lukeschaefer@gmail.com>
2019-05-08 22:31:58 -0700

    asd

---
+++ cse143/LetterInventory/letterinv.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+import java.io.*;
+
+public class HW1 {
+
+	public static void main(String[] args){
+
+	}
+	
+	public static void LetterInventory(String data){
+
+	}
+
+	public int get(char letter){
+
+	}
+
+	public void set(char letter, int value){
+
+	}
+
+	public int size(){
+
+	}
+
+	public boolean isEmpty(){
+
+	}
+
+	public String toString(){
+
+	}
+
+	public LetterInventory add(LetterInventory other){
+
+	}
+
+	public LetterInventory subtract(LetterInventory other){
+		
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Hello!
+
+My name's Luke and I'm a programmer based in Seattle.  This repository is for Data Structures, Algorhithms, programming problems, and more.
